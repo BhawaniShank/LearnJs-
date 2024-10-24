a274f20fea879353539ee8a45acdc70313121f29
Bhawanishankar <swastikin1@gmail.com>
2024-10-25 05:09:59 +0530

    day1

---
+++ 01_basics/02DataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // Treat all js code as newer version
+// alert(6+6) // We are using nodejs not browser 
+// console.log(6 + 6) console.log("Bhawani Shankar"); // to get error because we can use it in
+// two ways 1) by writing in next line or writing a semicolon between them it reduces code readability
+
+let name = "Shankar"
+let age = 19
+let isLoggedIn = false
+let state; 
+
+//number => 2 to power 53
+//bigint
+//string  => ""
+// boolean => true/false
+// null => standalone value 
+// undefined => 
+// symbol => unique 
+//object 
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object

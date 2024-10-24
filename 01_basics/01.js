5d557c0c657276fa7af6fa3a2ae20da749b8727a
Bhawanishankar <swastikin1@gmail.com>
2024-10-25 04:15:37 +0530

    Update 01.js

--- 01_basics/01.js
+++ 01_basics/01.js
@@ -1,2 +1,14 @@
 const accountId = 144553
 let accountEmail = "Shankar@gmail.com"
+var accountPass = "1245"
+accountCity = "Jaipur"  // variable created but not preferred
+
+// accountId = 2   // Assignment to constant variable not allowed
+
+accountEmail = "swastik@gmail.com"
+accountPass = "7364786"
+accountCity = "Mumbai" 
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPass, accountCity])
+/* Prefer not to use var because of issue in block scope and functional Scope */
